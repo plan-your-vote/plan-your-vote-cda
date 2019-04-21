ea7f6b1f175f19871854d22222403266b9e4eda4
Amy Hong <amyhong0502@gmail.com>
2019-04-21 12:33:01 -0700

    chore: add route constants

---
+++ src/constants/routes.js
@@ -0,0 +1,6 @@
+export const FRONTPAGE = '/';
+export const SELECTION = '/selection';
+export const CANDIDATES = '/selection/candidates';
+export const CAPITAL = '/selection/capital-plan';
+export const SCHEDULE = '/selection/schedule';
+export const REVIEW = '/selection/review';
