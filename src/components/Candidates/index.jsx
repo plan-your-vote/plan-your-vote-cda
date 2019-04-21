60d4cd1febad3924aaec623592f6d54da17c0c0b
Amy Hong <amyhong0502@gmail.com>
2019-04-21 14:20:00 -0700

    feat: implement Candidates table with dummy data

--- src/components/Candidates/index.jsx
+++ src/components/Candidates/index.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import Table from '../Table';
+import dummydata from '../../constants/dummyData/candidates.json';
 
 const Candidates = () => {
-  return <div>Candidates</div>;
+  return (
+    <div>
+      Candidates
+      <Table head={dummydata.head} body={dummydata.body} />
+    </div>
+  );
 };
 
 export default Candidates;
