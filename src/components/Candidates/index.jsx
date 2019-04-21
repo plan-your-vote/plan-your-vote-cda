4a796812928af67054162dab69ec9435440ccb3f
Amy Hong <amyhong0502@gmail.com>
2019-04-21 13:32:26 -0700

    chore: add selection components

---
+++ src/components/Candidates/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Candidates = () => {
+  return <div>Candidates</div>;
+};
+
+export default Candidates;
---
+++ src/components/Capital/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Capital = () => {
+  return <div>Capital</div>;
+};
+
+export default Capital;
