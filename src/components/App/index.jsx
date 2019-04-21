0ee4b778044a49e708cf1c6f323866f2179dd8f9
Amy Hong <amyhong0502@gmail.com>
2019-04-21 13:34:45 -0700

    feat: add /selection/:menu into the router and catch 404 page not found

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,20 +1,32 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Navigation from '../Navigation';
 import FrontPage from '../FrontPage';
 import Footer from '../Footer';
 import * as routes from '../../constants/routes';
+import SelectionPage from '../SelectionPage';
 
 class App extends Component {
   render() {
     return (
       <>
-      <Router>
-        <Navigation />
-        <Route exact path={routes.FRONTPAGE} component={() => <FrontPage />} />
-      </Router>
-      <Footer />
+        <Router>
+          <Navigation />
+          <Switch>
+            <Route
+              exact
+              path={routes.FRONTPAGE}
+              component={() => <FrontPage />}
+            />
+            <Route
+              path={routes.SELECTION}
+              component={() => <SelectionPage />}
+            />
+            <Route render={() => <h1>Page not found</h1>} />
+          </Switch>
+        </Router>
+        <Footer />
       </>
     );
   }
