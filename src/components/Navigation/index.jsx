a93599f5b8851a77a5b5a4ee75352c7b9434843c
Amy Hong <amyhong0502@gmail.com>
2019-04-22 15:28:12 -0700

    chore: sanitize import path

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import * as routes from '../../constants/routes';
+import * as routes from 'constants/routes';
 
 const Navigation = () => (
   <nav>
