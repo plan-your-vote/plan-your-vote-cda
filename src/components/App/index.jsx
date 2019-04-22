d8f4ce2b816008393f3e04d65e4bb8460ee11e34
Amy Hong <amyhong0502@gmail.com>
2019-04-22 13:55:44 -0700

    feat: get theme data from pyv backend

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import pyv from 'utils/api/pyv';
 import * as routes from 'constants/routes';
 import Navigation from 'components/Navigation';
 import Footer from 'components/Footer';
@@ -9,21 +10,41 @@ import Home from 'pages/home';
 import Selection from 'pages/selection';
 
 class App extends Component {
+  state = {
+    themeName: null,
+    images: [
+      {
+        id: '',
+        type: '',
+        value: '',
+        description: '',
+        format: ''
+      }
+    ]
+  };
+
+  componentDidMount() {
+    this.loadApiData();
+  }
+
+  loadApiData = async () => {
+    const response = await pyv.get('/theme');
+    const data = response.data;
+
+    this.setState({
+      themeName: data.selectedTheme.themeName,
+      images: data.images
+    });
+  };
+
   render() {
     return (
       <>
         <Router>
           <Navigation />
           <Switch>
-            <Route
-              exact
-              path={routes.HOME}
-              component={() => <Home />}
-            />
-            <Route
-              path={routes.SELECTION}
-              component={() => <Selection />}
-            />
+            <Route exact path={routes.HOME} component={() => <Home />} />
+            <Route path={routes.SELECTION} component={() => <Selection />} />
             <Route render={() => <h1>Page not found</h1>} />
           </Switch>
           <Footer />
