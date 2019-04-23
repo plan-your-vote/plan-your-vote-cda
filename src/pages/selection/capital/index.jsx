b083c0c9b7b03127a4f9a1fbf11a6c11e65fae80
Amy Hong <amyhong0502@gmail.com>
2019-04-23 09:05:31 -0700

    feat: get ballot issues data from backend

--- src/pages/selection/capital/index.jsx
+++ src/pages/selection/capital/index.jsx
@@ -1,33 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import MultipleChoiceQuestion from 'components/MultipleChoiceQuestion';
 import SectionHeader from 'components/SectionHeader';
+
+import pyv from 'utils/api/pyv';
+
 import dummyData from 'constants/dummyData/multipleChoice.json';
 import dummyHeader from 'constants/dummyData/pages.json';
 
-const Capital = () => {
-  const multipleChoice = dummyData.map(question => {
-    return (
-      <MultipleChoiceQuestion
-        key={question.name}
-        title={question.title}
-        description={question.description}
-        name={question.name}
-        values={question.values}
-      />
-    );
-  });
+class Capital extends Component {
+  state = {};
 
-  return (
-    <>
-      <SectionHeader
-        title={dummyHeader[1].title}
-        subtitle={dummyHeader[1].subtitle}
-        level='1'
-        description={dummyHeader[1].description}
-      />
-      {multipleChoice}
-    </>
-  );
-};
+  componentDidMount() {
+    this.loadApiData();
+  }
+
+  loadApiData = async () => {
+    const response = await pyv.get('/ballotissues');
+    const data = response.data;
+    console.log(data);
+  };
+
+  render() {
+    const multipleChoice = dummyData.map(question => {
+      return (
+        <MultipleChoiceQuestion
+          key={question.name}
+          title={question.title}
+          description={question.description}
+          name={question.name}
+          values={question.values}
+        />
+      );
+    });
+
+    return (
+      <>
+        <SectionHeader
+          title={dummyHeader[1].title}
+          subtitle={dummyHeader[1].subtitle}
+          level='1'
+          description={dummyHeader[1].description}
+        />
+        {multipleChoice}
+      </>
+    );
+  }
+}
 
 export default Capital;
