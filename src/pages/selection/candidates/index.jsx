a4476b46c5a29fc88ed742ebc33982ef555a6b01
Amy Hong <git@amyhong.dev>
2019-04-26 11:26:40 -0700

    feat: convert Candidates component from a constant variable to a class

--- src/pages/selection/candidates/index.jsx
+++ src/pages/selection/candidates/index.jsx
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Table from 'components/Table';
 import SectionHeader from 'components/SectionHeader';
 import dummyCandidates from 'constants/dummyData/candidates.json';
 import dummyHeader from 'constants/dummyData/pages.json';
 
-const Candidates = () => {
-  return (
-    <>
-      <SectionHeader
-        title={dummyHeader[0].title}
-        subtitle={dummyHeader[0].subtitle}
-        level='1'
-        description={dummyHeader[0].description}
-      />
-      <Table head={dummyCandidates.head} body={dummyCandidates.body} />
-    </>
-  );
-};
+class Candidates extends Component {
+  myfunction = () => {
+    // start from here
+  };
+
+  render() {
+    return (
+      <>
+        <SectionHeader
+          title={dummyHeader[0].title}
+          subtitle={dummyHeader[0].subtitle}
+          level='1'
+          description={dummyHeader[0].description}
+        />
+        <Table head={dummyCandidates.head} body={dummyCandidates.body} />
+      </>
+    );
+  }
+}
 
 export default Candidates;
