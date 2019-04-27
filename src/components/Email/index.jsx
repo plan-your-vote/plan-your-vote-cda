5ea0a38f337e3da6c5f9e3832c6ac31adc3a5616
Elizabeth Lee <quotable01@gmail.com>
2019-04-27 12:03:13 -0700

    Placed email component in table

--- src/components/Email/index.jsx
+++ src/components/Email/index.jsx
@@ -3,30 +3,53 @@ import React from 'react';
 const Email = () => {
   return (
     <>
-      <label htmlFor='email'>E-mail Address</label>
-      <input
-        id='email'
-        aria-describedby='email'
-        aria-required='true'
-        type='text'
-        name='email'
-      />
-      <label htmlFor='subject'>Subject</label>
-      <input
-        id='subject'
-        aria-describedby='subject'
-        type='text'
-        name='subject'
-      />
-      <label htmlFor='message'>Message:</label>
-      <input
-        id='message'
-        aria-describedby='message'
-        type='text'
-        name='message'
-      />
-
-      <button aria-label='send email'>Send Reminder Email</button>
+      <div>
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                <label htmlFor='email'>E-mail Address:</label>
+              </td>
+              <td>
+                <input
+                  id='email'
+                  aria-describedby='email'
+                  aria-required='true'
+                  type='text'
+                  name='email'
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label htmlFor='subject'>Subject:</label>
+              </td>
+              <td>
+                <input
+                  id='subject'
+                  aria-describedby='subject'
+                  type='text'
+                  name='subject'
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label htmlFor='message'>Message:</label>
+              </td>
+              <td>
+                <input
+                  id='message'
+                  aria-describedby='message'
+                  type='text'
+                  name='message'
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <button className="btn" aria-label='send email'>Send Reminder Email</button>
+      </div>
     </>
   );
 };
