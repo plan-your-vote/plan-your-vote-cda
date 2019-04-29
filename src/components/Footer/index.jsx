c47e11f68899084620ebf0b72cbefcf2de54671d
Elizabeth Lee <quotable01@gmail.com>
2019-04-29 11:25:59 -0700

    Changed class to className for react compatibility

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -24,17 +24,17 @@ const Footer = ({ logo }) => {
           </div>
           <div className='col-md-6'>
             <ul id='footer-share'>
-              <li id='twitter' class='fa-stack'>
-                <i class='fas fa-circle fa-stack-2x' />
-                <i class='fab fa-twitter fa-stack-1x fa-inverse' />
+              <li id='twitter' className='fa-stack'>
+                <i className='fas fa-circle fa-stack-2x' />
+                <i className='fab fa-twitter fa-stack-1x fa-inverse' />
               </li>
-              <li id='facebook' class='fa-stack'>
-                <i class='fas fa-circle fa-stack-2x' />
-                <i class='fab fa-facebook-f fa-stack-1x fa-inverse' />
+              <li id='facebook' className='fa-stack'>
+                <i className='fas fa-circle fa-stack-2x' />
+                <i className='fab fa-facebook-f fa-stack-1x fa-inverse' />
               </li>
-              <li id='linkedin' class='fa-stack'>
-                <i class='fas fa-circle fa-stack-2x' />
-                <i class='fab fa-linkedin-in fa-stack-1x fa-inverse' />
+              <li id='linkedin' className='fa-stack'>
+                <i className='fas fa-circle fa-stack-2x' />
+                <i className='fab fa-linkedin-in fa-stack-1x fa-inverse' />
               </li>
             </ul>
             <select
