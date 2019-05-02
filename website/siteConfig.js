e1761beec2a9883a360edecb0a1e9d392400b859
Amy Hong <git@amyhong.dev>
2019-05-02 01:23:41 -0700

    feat: configure website

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -8,40 +8,48 @@
 // See https://docusaurus.io/docs/site-config for all the possible
 // site configuration options.
 
+const PROJECT_NAME = 'Plan Your Vote';
+
 // List of projects/orgs using your project for the users page.
 const users = [
   {
-    caption: 'User1',
+    caption: 'City of Foo',
     // You will need to prepend the image path with your baseUrl
     // if it is not '/', like: '/test-site/img/image.jpg'.
-    image: '/img/undraw_open_source.svg',
-    infoLink: 'https://www.facebook.com',
-    pinned: true,
+    image: '/img/undraw_open_source.svg', // TODO
+    infoLink: 'https://www.facebook.com', // TODO
+    pinned: true
   },
+  {
+    caption: 'Bar Highschool',
+    image: '/img/undraw_open_source.svg', // TODO
+    infoLink: 'https://www.facebook.com', // TODO
+    pinned: true
+  }
 ];
 
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
-  tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  title: PROJECT_NAME, // Title for your website.
+  tagline: 'A website for testing', // TODO
+  url: 'https://your-docusaurus-test-site.com', // Your website URL // TODO
+  baseUrl: '/', // Base URL for your project */ // TODO
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'facebook',
+  projectName: PROJECT_NAME,
+  organizationName: 'facebook', // TODO
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'doc1', label: 'Docs'},
-    {doc: 'doc4', label: 'API'},
-    {page: 'help', label: 'Help'},
-    {blog: true, label: 'Blog'},
+    { doc: 'doc1', label: 'Docs' },
+    { doc: 'doc4', label: 'API' },
+    { page: 'help', label: 'Help' },
+    { blog: true, label: 'Blog' }
   ],
 
   // If you have users set above, you add it here:
@@ -55,7 +63,7 @@ const siteConfig = {
   /* Colors for website */
   colors: {
     primaryColor: '#35489e',
-    secondaryColor: '#25326e',
+    secondaryColor: '#25326e'
   },
 
   /* Custom fonts for website */
@@ -73,11 +81,11 @@ const siteConfig = {
   */
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
+  copyright: `Copyright © ${new Date().getFullYear()} ${PROJECT_NAME}`,
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
-    theme: 'default',
+    theme: 'default'
   },
 
   // Add custom scripts here that would be placed in <script> tags.
@@ -96,7 +104,7 @@ const siteConfig = {
   // enableUpdateBy: true,
 
   // Show documentation's last update time.
-  // enableUpdateTime: true,
+  enableUpdateTime: true
 
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
