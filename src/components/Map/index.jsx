2b338cc40d6f529bb818aa499e2bfcb6797002e7
Amy Hong <git@amyhong.dev>
2019-05-06 13:16:45 -0700

    rendered the map

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
+import mapboxgl from 'mapbox-gl';
 
-const Map = () => {
-  return <>Map</>;
-};
+import { MAPBOX } from 'credentials.js';
+
+class Map extends Component {
+  map;
+
+  componentDidMount() {
+    mapboxgl.accessToken = MAPBOX;
+
+    this.map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v9',
+      center: [-123.1139269, 49.2608838],
+      zoom: 12
+    });
+  }
+
+  render() {
+    return <div id='map' style={{ width: '100%', height: '500px' }} />;
+  }
+}
 
 export default Map;
