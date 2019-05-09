add147ef05942984711685ed0090dc9e27db7738
Amy Hong <git@amyhong.dev>
2019-05-09 10:21:49 -0700

    rendered data from the CMS API and removed dummy data

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -2,41 +2,84 @@ import React, { Component } from 'react';
 import mapboxgl from 'mapbox-gl';
 
 import { MAPBOX } from 'credentials.js';
-import coordinates from 'constants/coordinates.json';
+import pyv from 'utils/api/pyv';
 
 class Map extends Component {
-  map;
+  _map;
+  _isMounted = false;
 
   state = {
+    pollingStations: [
+      {
+        additionalInfo: '',
+        address: '',
+        election: null,
+        electionId: 1,
+        generalAccessInfo: null,
+        latitude: 0,
+        longitute: 0, // TODO longitude
+        name: '',
+        parkingInfo: null,
+        pollingStationId: 0,
+        washroomInfo: null,
+        wheelchairInfo: null
+      }
+    ],
     markers: []
   };
 
   componentDidMount() {
+    this._isMounted = true;
     mapboxgl.accessToken = MAPBOX;
 
-    this.map = new mapboxgl.Map({
+    this._map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [-123.1139269, 49.2608838],
       zoom: 13
     });
 
-    coordinates.map(address => {
-      return this.addMarker(address.latitude, address.longitude);
-    });
+    this.loadApiData()
+      .then(data => {
+        if (this._isMounted) {
+          this.setState({
+            pollingStations: data.pollingStations
+          });
+        }
+      })
+      .then(() => {
+        this.renderMarkers();
+      });
   }
+
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
+
+  loadApiData = async () => {
+    const response = await pyv.get('/api/PollingStations');
+    return response.data;
+  };
+
+  renderMarkers = () => {
+    this.state.pollingStations.map(pollingStation => {
+      return this.addMarker(pollingStation.latitude, pollingStation.longitute);
+    });
+  };
 
   addMarker = (latitude, longitude) => {
     const marker = new mapboxgl.Marker()
       .setLngLat([longitude, latitude])
-      .addTo(this.map);
+      .addTo(this._map);
 
     const currentMarkers = this.state.markers;
     currentMarkers.push(marker);
 
-    this.setState({
-      markers: currentMarkers
-    });
+    if (this._isMounted) {
+      this.setState({
+        markers: currentMarkers
+      });
+    }
   };
 
   render() {
