cca63cd1bb3c95a5214c03b5049844252ffeb026
Amy Hong <git@amyhong.dev>
2019-05-15 18:55:33 -0700

    refactored ICS component

--- src/components/ICS/index.jsx
+++ src/components/ICS/index.jsx
@@ -1,30 +1,29 @@
 import React from 'react';
 
+const dateSelector = dates => {
+  const options = dates.map(date => {
+    return <option aria-describedby='info'>{date}</option>;
+  });
+
+  return <select id='dateSelector'>{options}</select>;
+};
+
 const ICS = () => {
   return (
     <div>
       <label
         htmlFor='dateSelector'
         aria-label='format: year month day'
-        className="col-form-label"
-        id="info"
+        className='col-form-label'
+        id='info'
       >
         Date:
       </label>
-      <select id='dateSelector'>
-        <option aria-describedby='info'>2019-04-15</option>
-        <option aria-describedby='info'>2019-05-23</option>
-        <option aria-describedby='info'>2019-06-28</option>
-      </select>
-
-      <div
-        id="addtocalendar"
-      >
-        Add to Calendar
-      </div>
+      {dateSelector(['2019-04-15', '2019-05-23', '2019-06-28'])}
+      <div id='addtocalendar'>Add to Calendar</div>
       <button
-        aria-label="Generating and downloading pdf summary of your plan. Remember to officially vote in person on election day."
-        className="btn btn-secondary"
+        aria-label='Generating and downloading pdf summary of your plan. Remember to officially vote in person on election day.'
+        className='btn btn-secondary'
       >
         Generate Voting Plan PDF
       </button>
