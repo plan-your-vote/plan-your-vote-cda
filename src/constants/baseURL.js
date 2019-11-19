64f06c47ec94886436e9a23046a755129b4904bc
PavalSidhu <pavalsidhu@hotmail.com>
2019-11-19 10:39:42 -0800

    fixed based url

--- src/constants/baseURL.js
+++ src/constants/baseURL.js
@@ -1,5 +1,5 @@
 //export const CMS_BASE_URL = 'https://dpyvweb.azurewebsites.net';
-// export const CMS_MAP_BASE_URL = 'https://planyourvote-cms.azurewebsites.net';
-// export const CMS_BASE_URL = 'https://planyourvote-cms.azurewebsites.net';
-export const CMS_BASE_URL = 'https://localhost:5001';
-export const CMS_MAP_BASE_URL = 'https://localhost:5001';
+export const CMS_MAP_BASE_URL = 'https://planyourvote-cms.azurewebsites.net';
+export const CMS_BASE_URL = 'https://planyourvote-cms.azurewebsites.net';
+// export const CMS_BASE_URL = 'https://localhost:5001';
+// export const CMS_MAP_BASE_URL = 'https://localhost:5001';
