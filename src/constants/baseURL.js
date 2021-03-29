7eb97373ef73cb543208f7a57e1a20f89c8fd9a5
Kelvin Lau <GEJ26@vancouver.ca>
2021-03-29 14:44:14 -0700

    change the base URL

--- src/constants/baseURL.js
+++ src/constants/baseURL.js
@@ -1,5 +1,5 @@
 //export const CMS_BASE_URL = 'https://dpyvweb.azurewebsites.net';
 // export const CMS_MAP_BASE_URL = 'https://localhost/admin';
 // export const CMS_BASE_URL = 'https://localhost/admin';
-export const CMS_BASE_URL = 'https://pyv-cms.azurewebsites.net/';
-export const CMS_MAP_BASE_URL = 'https://pyv-cms.azurewebsites.net/';
+export const CMS_BASE_URL = 'https://pyvcms.azurewebsites.net/';
+export const CMS_MAP_BASE_URL = 'https://pyvcms.azurewebsites.net/';
